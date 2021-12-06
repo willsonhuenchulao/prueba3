feb1f7e8fb1a0b4d427d62bb62461e2528fad960
willsonhuenchulao <willsonnuevo1@gmail.com>
2021-12-06 15:05:26 -0400

    editando

--- contenido.php
+++ contenido.php
@@ -21,7 +21,7 @@
 <div class="container-fluid bg-inverse fixed-top">
 <nav class="navbar navbar-expand-lg navbar-light bg-light navbar navbar-dark bg-dark container">
   <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">MuySocial/a>
+    <a class="navbar-brand" href="index.php">MuySocial</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -30,27 +30,15 @@
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="index.php">i</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">hola</a>
+        
+        
+          
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
+       
       </ul>
       <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success mr-4" type="submit">Search</button>
+        <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
+        <button class="btn btn-outline-success mr-4" type="submit">Buscar</button>
       </form>
       <div><a href="login.php" class="btn btn-danger mr-2">Login</a></div>
       <div><a href="#" class="btn btn-danger">Registrarse</a></div>
@@ -68,9 +56,9 @@
 
     <div class="cont-menu active" id="menu">
         <ul>
-            <li>home</li>
-            <li>about</li>
-            <li>contact</li>
+            <li><a href="index.php"></a> Inicio</li>
+            
+            <li><a href="login.php"></a> </li>
             <li>show</li>
         </ul>
     </div>
