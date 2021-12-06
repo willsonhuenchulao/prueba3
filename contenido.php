06bb37877d09dfbea32ed163e34b596f1c770c2a
willsonhuenchulao <willsonnuevo1@gmail.com>
2021-12-06 09:57:51 -0400

    cambio el texto

--- contenido.php
+++ contenido.php
@@ -31,7 +31,7 @@
           <a class="nav-link active" aria-current="page" href="index.php">i</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#">hola</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
