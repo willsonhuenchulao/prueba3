714a280466a659a45d8e932b860feec8e048a281
willsonhuenchulao <willsonnuevo1@gmail.com>
2021-12-06 15:04:53 -0400

    editando

---
+++ noticias.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/noticias.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <h1>hola</h1>
+    <nav>
+        <ul>
+            <li>menu</li>
+            <li>menu</li>
+            <li>menu</li>
+            <li>menu</li>
+        </ul>
+    </nav>
+    <div class="contenido">
+        <div class="articulos">
+            <div class="secciones">
+                <div>
+                    <h2>subtitulos</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit blanditiis inventore voluptate, sunt, saepe aspernatur ducimus ullam et tempora nisi consequatur libero fuga, ipsa eveniet molestias placeat nobis adipisci ut.</p>
+                </div>
+                <div><img src="img/superheroe.jpg" alt=""></div>
+            </div>
+        </div>
+        <div class="menu-secundario">
+            <a href="">Mas articulos</a>
+            <a href="">Mas articulos</a>
+            <a href="">Mas articulos</a>
+            <a href="">Mas articulos</a>
+            <a href="">Mas articulos</a>
+            <a href="">Mas articulos</a>
+        </div>
+    </div>
+    <div class="contenido">
+        <div class="articulos">
+            <div class="secciones">
+                <div>
+                    <h2>subtitulos</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit blanditiis inventore voluptate, sunt, saepe aspernatur ducimus ullam et tempora nisi consequatur libero fuga, ipsa eveniet molestias placeat nobis adipisci ut.</p>
+                </div>
+                <div><img src="img/superheroe.jpg" alt=""></div>
+            </div>
+        </div>
+        <div class="menu-secundario">
+            <a href="">Mas articulos</a>
+            <a href="">Mas articulos</a>
+            <a href="">Mas articulos</a>
+            <a href="">Mas articulos</a>
+            <a href="">Mas articulos</a>
+            <a href="">Mas articulos</a>
+        </div>
+    </div>
+    <footer>
+    <center><p>MuySocial | 2021.</p></center>
+</footer>
+</body>
+</html>
